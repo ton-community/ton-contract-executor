7aacc25442383f0333ca52819e3b8c5d5a7d87e8
altox <xeroxaltox@gmail.com>
2022-03-25 21:16:43 +0300

    wip: some optimizations

--- src/executor/SmartContract.ts
+++ src/executor/SmartContract.ts
@@ -1,5 +1,13 @@
 import {Cell, ExternalMessage, InternalMessage, Slice} from "ton";
-import {buildC7, C7Config, runContract, TVMStack, TVMStackEntry, TVMStackEntryTuple} from "./executor";
+import {
+    buildC7,
+    C7Config,
+    getSelectorForMethod,
+    runTVM,
+    TVMStack,
+    TVMStackEntry,
+    TVMStackEntryTuple
+} from "./executor";
 import {compileFunc} from "ton-compiler";
 import BN from "bn.js";
 
@@ -74,6 +82,8 @@ const decodeLogs = (logs: string) => Buffer.from(logs, 'base64').toString()
 export class SmartContract {
     public codeCell: Cell
     public dataCell: Cell
+    private codeCellBoc: string
+    private dataCellBoc: string
     private config: SmartContractConfig
     private c7Config: C7Config = {}
     private c7: TVMStackEntryTuple|null = null
@@ -81,24 +91,35 @@ export class SmartContract {
     private constructor(codeCell: Cell, dataCell: Cell, config?: SmartContractConfig) {
         this.codeCell = codeCell
         this.dataCell = dataCell
+        this.codeCellBoc = codeCell.toBoc({ idx: false }).toString('base64')
+        this.dataCellBoc = dataCell.toBoc({ idx: false }).toString('base64')
         this.config = config || { getMethodsMutate: false }
     }
 
+    private async runContract(method: string, stack: TVMStack) {
+        let executorConfig = {
+            function_selector: getSelectorForMethod(method),
+            init_stack: stack,
+            code: this.codeCellBoc,
+            data: this.dataCellBoc,
+            c7_register: this.getC7()
+        }
+        let res = await runTVM(executorConfig)
+        if (res.ok === false) {
+            throw new Error('Cant execute vm: ' + res.error)
+        }
+        return res
+    }
+
     async invokeGetMethod(method: string, args: TVMStack): Promise<ExecutionResult> {
-        let res = await runContract(
-            this.codeCell,
-            this.dataCell,
-            args,
-            method,
-            this.getC7()
-        )
+        let res = await this.runContract(method, args)
 
         if (res.exit_code !== 0) {
             return { type: 'failed', exit_code: res.exit_code, result: [] as NormalizedStackEntry[], logs: decodeLogs(res.logs) }
         }
 
         if (this.config.getMethodsMutate && res.data_cell) {
-            this.dataCell = bocToCell(res.data_cell)
+            this.setDataCell(bocToCell(res.data_cell))
         }
 
         return {
@@ -122,25 +143,19 @@ export class SmartContract {
         let bodyCell = new Cell()
         message.body.body.writeTo(bodyCell)
 
-        let res = await runContract(
-            this.codeCell,
-            this.dataCell,
-            [
-                {type: 'int', value: '1000'},                           // smc_balance
-                {type: 'int', value: message.value.toString(10)}, // msg_value
-                {type: 'cell', value: await cellToBoc(msgCell)},        // msg cell
-                {type: 'cell_slice', value: await cellToBoc(bodyCell)}, // body slice
-            ],
-            'recv_internal',
-            this.getC7()
-        )
+        let res = await this.runContract('recv_internal', [
+            {type: 'int', value: '1000'},                           // smc_balance
+            {type: 'int', value: message.value.toString(10)}, // msg_value
+            {type: 'cell', value: await cellToBoc(msgCell)},        // msg cell
+            {type: 'cell_slice', value: await cellToBoc(bodyCell)}, // body slice
+        ])
 
         if (res.exit_code !== 0) {
             return { type: 'failed', exit_code: res.exit_code, result: [] as NormalizedStackEntry[], logs: decodeLogs(res.logs) }
         }
 
         if (res.data_cell) {
-            this.dataCell = bocToCell(res.data_cell)
+            this.setDataCell(bocToCell(res.data_cell))
         }
 
         // TODO: handle code update
@@ -166,25 +181,19 @@ export class SmartContract {
         let bodyCell = new Cell()
         message.body.body.writeTo(bodyCell)
 
-        let res = await runContract(
-            this.codeCell,
-            this.dataCell,
-            [
-                {type: 'int', value: '1000'},                           // smc_balance
-                {type: 'int', value: '0'},                              // msg_value
-                {type: 'cell', value: await cellToBoc(msgCell)},        // msg cell
-                {type: 'cell_slice', value: await cellToBoc(bodyCell)}, // body slice
-            ],
-            'recv_external',
-            this.getC7()
-        )
+        let res = await this.runContract('recv_external', [
+            {type: 'int', value: '1000'},                           // smc_balance
+            {type: 'int', value: '0'},                              // msg_value
+            {type: 'cell', value: await cellToBoc(msgCell)},        // msg cell
+            {type: 'cell_slice', value: await cellToBoc(bodyCell)}, // body slice
+        ])
 
         if (res.exit_code !== 0) {
             return { type: 'failed', exit_code: res.exit_code, result: [] as NormalizedStackEntry[], logs: decodeLogs(res.logs) }
         }
 
         if (res.data_cell) {
-            this.dataCell = bocToCell(res.data_cell)
+            this.setDataCell(bocToCell(res.data_cell))
         }
 
         // TODO: handle code update
@@ -217,6 +226,11 @@ export class SmartContract {
         } else {
             return buildC7(this.c7Config)
         }
+    }
+
+    setDataCell(dataCell: Cell) {
+        this.dataCell = dataCell
+        this.dataCellBoc = dataCell.toBoc({ idx: false }).toString('base64')
     }
 
     static async fromFuncSource(source: string, dataCell: Cell, config?: SmartContractConfig) {
